2655fd66965ce21489be0ecef17b541bb83c3e28
lipeng <lipeng.sh@gmail.com>
2020-04-02 17:06:15 +0800

    todo:groupby

--- tests/pandas.rs
+++ tests/pandas.rs
@@ -1,10 +1,12 @@
 use csv::ReaderBuilder;
+use itertools::Itertools;
 use serde::Deserialize;
 use std::error::Error;
 use time::Instant;
 
 struct DataFrame {
     header: csv::StringRecord,
+    index: Vec<String>,
     dimension: Vec<String>,
     tag: Vec<String>,
     prf: Vec<f32>,
@@ -15,6 +17,7 @@ impl DataFrame {
     fn new() -> DataFrame {
         DataFrame {
             header: csv::StringRecord::new(),
+            index: Vec::new(),
             dimension: Vec::new(),
             tag: Vec::new(),
             prf: Vec::new(),
@@ -22,8 +25,9 @@ impl DataFrame {
         }
     }
 
-    fn push(&mut self, row: Row) {
+    fn push(&mut self, row: Row, index_value: String) {
         self.dimension.push(row.dimension);
+        self.index.push(index_value);
         self.tag.push(row.tag);
         self.prf.push(row.prf);
         self.mkt.push(row.mkt);
@@ -55,12 +59,18 @@ fn read_csv() -> Result<(), Box<dyn Error>> {
 
     while let Some(result) = iter.next() {
         let record: Row = result?;
-        println!("row {:?}", record);
-        dataframe.push(record);
+        // println!("row {:?}", record);
+        let index_value: String = record.dimension.clone() + record.tag.clone().as_str();
+        dataframe.push(record, index_value);
     }
 
     println!("time elapse:{}", start.elapsed().as_seconds_f32());
     println!("dataframe: {}", dataframe.dimension.len());
+
+    for (key, group) in &dataframe.index.into_iter().group_by() {
+        println!("key:{},group:{}", key, group);
+    }
+
     Ok(())
 }
 
