20d653b07c763b4a72595456a8cf5d61d139d59f
lipeng <lipeng.sh@gmail.com>
2020-04-03 19:37:04 +0800

    todo:add hash map

---
+++ src/pandas/index.rs
@@ -0,0 +1,5 @@
+/// index and multi-index
+
+pub struct Index {
+
+}
--- src/pandas/mod.rs
+++ src/pandas/mod.rs
@@ -4,6 +4,7 @@ mod series;
 mod dataframe;
 mod dtypes;
 mod columns;
+mod index;
 
 pub use groupby::Groupby;
 pub use series::Series;
