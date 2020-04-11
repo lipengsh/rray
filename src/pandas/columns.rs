cdf594180250cf3873b0a57bc368c26058412c5c
lipeng <lipeng.sh@gmail.com>
2020-04-11 16:17:41 +0800

    Index ,one two ,u32 string

--- src/pandas/columns.rs
+++ src/pandas/columns.rs
@@ -3,6 +3,13 @@ use arrow::datatypes::DataType;
 
 #[warn(unused_variables)]
 pub struct Columns {
-    name: String,
-    column_type: DataType,
+    pub name: String,
+    pub column_type: DataType,
+    /// column in data,
+    /// data = {'Name':['Jai', 'Princi', 'Gaurav', 'Anuj'],
+    ///         'Age':[27, 24, 22, 32],
+    ///         'Address':['Delhi', 'Kanpur', 'Allahabad', 'Kannauj'],
+    ///         'Qualification':['Msc', 'MA', 'MCA', 'Phd']}
+    /// column name : Age, column data_index:1
+    pub data_index: u32,
 }
--- src/pandas/dataframe.rs
+++ src/pandas/dataframe.rs
@@ -1,7 +1,9 @@
+use crate::pandas::index::{HashIndex, Index};
 use crate::pandas::Columns;
-// use crate::pandas::Index;
+use arrow::array;
 use arrow::array::Array;
-// use std::sync::Arc;
+use arrow::datatypes::DataType;
+use rayon::prelude::*;
 
 /// dataframe struct and impl
 pub struct Dataframe {
@@ -12,11 +14,52 @@ pub struct Dataframe {
     ///         'Qualification':['Msc', 'MA', 'MCA', 'Phd']}
     /// dyn trait(Index) is dynamic dispatch for polymorphism situation
     /// because trait has impl ,trait is dynamic
-    data: Option<Vec<Box<dyn Array>>>,
+    data: Option<Vec<Box<dyn array::Array>>>,
     columns_name: Option<Vec<Columns>>,
-    row_length: Option<u32>,
-    column_length: Option<u32>,
+    row_length: u32,
+    column_length: u32,
     /// index trait
     /// box is a pointer to value in heap, pointer is in stack
+    /// hash index
     index: Option<Vec<u64>>,
+}
+
+impl Dataframe {
+    fn new() -> Dataframe {
+        Dataframe {
+            data: None,
+            columns_name: None,
+            row_length: 0,
+            column_length: 0,
+            index: None,
+        }
+    }
+
+    fn set_index(&self, columns: &[Columns]) -> bool {
+        if self.column_length == 0 {
+            // todo: set warning message log
+            return false;
+        }
+
+        // set hash array
+        let mut index: HashIndex = Index::new();
+
+        if columns.len() == 1 {
+            match columns[0].column_type {
+                DataType::UInt8 | DataType::UInt16 | DataType::UInt32 | DataType::UInt64 => {
+                    {
+                        // get column array
+                        let r = &self.data.as_ref().unwrap()[columns[0].data_index as usize];
+                        let u_array = r.as_any().downcast_ref::<array::UInt32Array>().unwrap();
+                        // todo : Uint32Array to &[u32]
+                    };
+                }
+                _ => {
+                    {};
+                }
+            }
+        }
+
+        true
+    }
 }
