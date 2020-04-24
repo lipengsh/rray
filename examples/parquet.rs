bab66e4901170e85d5183e21060fd572f1c7ce77
lipeng <lipeng.sh@gmail.com>
2020-04-24 15:00:50 +0800

    T::T kao

--- examples/parquet.rs
+++ examples/parquet.rs
@@ -3,17 +3,17 @@ extern crate rray;
 use parquet::basic::Repetition::REQUIRED;
 use parquet::basic::Type::{BYTE_ARRAY, FLOAT};
 use parquet::basic::{Repetition, Type};
-use parquet::column::writer::ColumnWriter;
-use parquet::data_type::ByteArray;
+use parquet::column::writer::{get_column_writer, ColumnWriter};
+use parquet::data_type::{ByteArray, ByteArrayType, DataType};
 use parquet::file::properties::WriterProperties;
-use parquet::file::writer::{FileWriter, SerializedFileWriter};
+use parquet::file::writer::{FileWriter, RowGroupWriter, SerializedFileWriter};
+use parquet::memory::BufferPtr;
 use parquet::schema::types;
-use parquet::schema::types::PrimitiveTypeBuilder;
 use rray::pandas::utils::{gen_f32, gen_string};
 use std::rc::Rc;
 
 fn main() {
-    make_schema();
+    create_parquet();
 }
 
 fn create_column_schema(name: &str, ty: Type, repeat: Repetition) -> Rc<types::Type> {
@@ -40,81 +40,56 @@ fn create_parquet() {
 
     let props = Rc::new(WriterProperties::builder().build());
 
-    let file =
-        rray::parquet::utils::get_temp_file("test_row_group_writer_num_records_mismatch", &[]);
+    let file = rray::parquet::utils::get_temp_file("sample_parquet", &[]);
 
     // generate sample file
     let array_len = 1024;
     let row_group_size = 128;
 
-    let mut dim_array = gen_string(len);
-    let mut tag_array = gen_string(len);
-    let mut ptr_array = gen_f32(len);
-    let mut mkt_array = gen_f32(len);
+    let mut dim_array = gen_string(array_len);
+    let mut tag_array = gen_string(array_len);
+    let mut ptr_array = gen_f32(array_len);
+    let mut mkt_array: Vec<f32> = gen_f32(array_len);
+
+    let mut dim_byte_array: Vec<ByteArray> = dim_array
+        .iter()
+        .map(|x| ByteArray::from(x.as_str()))
+        .collect();
 
     // write to parquet file
     let mut writer = SerializedFileWriter::new(file.try_clone().unwrap(), schema, props).unwrap();
     let mut row_group_writer = writer.next_row_group().unwrap();
 
     // write to parquet as row_group_size
-    let offset = 0;
+    let mut offset = 0;
     while offset < 1024 {
         // write dim array
-        if let Some(mut col_writer) = row_group_writer.next_column().unwrap() {
-            match col_writer {
-                ColumnWriter::ByteArrayColumnWriter(ref mut typed) => {
-                    typed
-                        .write_batch(&dim_array[offset..row_group_size], None, None)
-                        .unwrap();
-                }
-                _ => unimplemented!(),
-            }
-        }
-
-        // write tag array
-        if let Some(mut col_writer) = row_group_writer.next_column().unwrap() {
-            match col_writer {
-                ColumnWriter::ByteArrayColumnWriter(ref mut typed) => {
-                    typed
-                        .write_batch(&tag_array[offset..row_group_size], None, None)
-                        .unwrap();
-                }
-                _ => unimplemented!(),
-            }
-        }
-
-        // write ptr array
-        if let Some(mut col_writer) = row_group_writer.next_column().unwrap() {
-            match col_writer {
-                ColumnWriter::ByteArrayColumnWriter(ref mut typed) => {
-                    typed
-                        .write_batch(&ptr_array[offset..row_group_size], None, None)
-                        .unwrap();
-                }
-                _ => unimplemented!(),
-            }
-        }
-
-        // write mkt array
-        if let Some(mut col_writer) = row_group_writer.next_column().unwrap() {
-            match col_writer {
-                ColumnWriter::ByteArrayColumnWriter(ref mut typed) => {
-                    typed
-                        .write_batch(&mkt_array[offset..row_group_size], None, None)
-                        .unwrap();
-                }
-                _ => unimplemented!(),
-            }
-        }
+        write_column::<ByteArrayType>(
+            &mut row_group_writer,
+            &dim_byte_array[offset..offset + row_group_size],
+        );
+        // write_column(&row_group_writer, &tag_array[offset..row_group_size]);
+        // write_column(&row_group_writer, &ptr_array[offset..row_group_size]);
+        // write_column(&row_group_writer, &mkt_array[offset..row_group_size]);
+        // let result = write_column(
+        //     &row_group_writer,
+        //     &mkt_array[offset..offset + row_group_size],
+        // );
+        offset += row_group_size;
+        println!("offset:{}", offset);
     }
-
-    writer.close_row_group().unwrap();
+    writer.close_row_group(row_group_writer).unwrap();
     writer.close().unwrap();
-
-    println!("schema:{:?}", schema);
 }
 
-fn make_schema() {
-    let column_schema = create_column_schema("dim", BYTE_ARRAY, REQUIRED);
-    println!("schema: {:?}", column_schema);
+fn tmp_write_column<T>(slice: &[T]) -> &[T] {
+    slice
+}
+
+fn write_column<T: DataType>(mut row_group_writer: &mut Box<dyn RowGroupWriter>, slice: &[T::T]) {
+    if let Some(mut col_writer) = row_group_writer.next_column().unwrap() {
+        parquet::column::writer::get_typed_column_writer_mut::<T>(&mut col_writer)
+            .write_batch(slice, None, None)
+            .unwrap();
+    }
 }
