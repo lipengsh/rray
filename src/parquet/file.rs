40016d5193c41c961f7164c57b9f5cdad0da7fe9
lipeng <lipeng.sh@gmail.com>
2020-04-28 18:26:36 +0800

    write parquet with difference type

--- src/parquet/file.rs
+++ src/parquet/file.rs
@@ -1,5 +1,5 @@
 use crate::parquet::format::Format;
-use parquet::file::writer::SerializedFileWriter;
+use parquet::file::writer::{FileWriter, SerializedFileWriter};
 use std::fs;
 use std::fs::File;
 use std::io;
@@ -39,13 +39,13 @@ impl FileHandler {
 }
 
 /// parquet writer
-pub struct FileWriter {
+pub struct ParquetWriter {
     writer_handler: SerializedFileWriter<File>,
 }
 
-impl FileWriter {
-    fn new(file_handler: FileHandler, format: Format) -> Self {
-        FileWriter {
+impl ParquetWriter {
+    pub fn new(file_handler: FileHandler, format: Format) -> Self {
+        ParquetWriter {
             writer_handler: SerializedFileWriter::new(
                 file_handler.try_clone().unwrap(),
                 format.schema,
@@ -53,5 +53,83 @@ impl FileWriter {
             )
             .unwrap(),
         }
+    }
+    //
+    // // write parquet
+    // pub fn write_parquet(&mut self, data: &Vec<Vec<dyn DataType>>) {
+    //     let mut row_group_writer = self.writer_handler.next_row_group().unwrap();
+    //
+    //     for item in &data {
+    //         if let Some(mut col_writer) = row_group_writer.next_column().unwrap() {
+    //             get_typed_column_writer_mut::<dyn DataType>(&mut col_writer)
+    //                 .write_batch(item, None, None)
+    //                 .unwrap();
+    //         }
+    //         row_group_writer.close_column().unwrap();
+    //     }
+    //     self.writer_handler.close_row_group().unwrap();
+    // }
+
+    // close writer
+    pub fn close(&mut self) {
+        self.writer_handler.close().unwrap();
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::pandas::utils::{gen_f32, gen_string};
+    use crate::parquet::file::{FileHandler, ParquetWriter};
+    use crate::parquet::format::{ColumnSchema, Format};
+    use parquet::basic::Type;
+    use parquet::data_type::ByteArray;
+
+    fn write_parquet() {
+        // create file handler
+        let file_handler = FileHandler::new("sample.parquet");
+
+        // create file format
+        let file_format = Format::new(
+            "sample",
+            vec![
+                ColumnSchema {
+                    name: "dim".to_string(),
+                    column_type: Type::BYTE_ARRAY,
+                },
+                ColumnSchema {
+                    name: "tag".to_string(),
+                    column_type: Type::BYTE_ARRAY,
+                },
+                ColumnSchema {
+                    name: "ptr".to_string(),
+                    column_type: Type::BOOLEAN,
+                },
+                ColumnSchema {
+                    name: "mkt".to_string(),
+                    column_type: Type::BOOLEAN,
+                },
+            ],
+        );
+
+        // generate sample data
+
+        let dim_array = gen_string(array_len);
+        let tag_array = gen_string(array_len);
+        let ptr_array: Vec<f32> = gen_f32(array_len);
+        let mkt_array: Vec<f32> = gen_f32(array_len);
+
+        let dim_byte_array: Vec<ByteArray> = dim_array
+            .iter()
+            .map(|x| ByteArray::from(x.as_str()))
+            .collect();
+        let tag_byte_array: Vec<ByteArray> = tag_array
+            .iter()
+            .map(|x| ByteArray::from(x.as_str()))
+            .collect();
+
+        let sample_data = [dim_byte_array, tag_byte_array, ptr_array, mkt_array];
+
+        // create ParquetWriter
+        // ParquetWriter::new(file_handler, file_format).write_parquet(&sample_data);
     }
 }
